25ef3014d58663ac70c166e4d3739d94b8d90a2b
Marcin <mlojek@protonmail.com>
2023-01-14 21:53:03 +0100

    add function cut_y

--- torch-lstm/process_data.py
+++ torch-lstm/process_data.py
@@ -1,6 +1,6 @@
-import os
-
 import pandas as pd
+
+Y_NAME = 'Indoor_temperature_room'
 
 
 def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
@@ -12,19 +12,16 @@ def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
     '''
     df.drop(['Id', 'Date', 'Time'], axis=1, inplace=True)
     df['Day_of_the_week'] = df['Day_of_the_week'].astype('int')
-    print(pd.unique(df['Day_of_the_week']))
     df = pd.get_dummies(df, columns=['Day_of_the_week'])
 
     return df
 
 
-if __name__ == '__main__':
-    raw_data = pd.read_csv(os.path.join(
-        '..', 'data', 'smart-home', 'train.csv'))
-    raw_data.drop(['Indoor_temperature_room'], axis=1, inplace=True)
-
-    processed = preprocess_data(raw_data)
-    print(processed.dtypes)
-
-    # print no of columns
-    print(processed.shape[1])
+def cut_y(df: pd.DataFrame, y_name: str) -> tuple:
+    '''
+    Cut Indoor_temperature_room from input dataframe, return a
+    tuple of dataframes (x, y)
+    '''
+    y = df[y_name]
+    x = df.drop([y_name], axis=1)
+    return x, y
